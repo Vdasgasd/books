3ef92631cf469a88223f3b58c3db00605e7097b3
Vdasgasd <Sandiaz.r.gultom@gmail.com>
2023-12-31 00:30:07 +0700

    get books again

--- src/app/books/page.js
+++ src/app/books/page.js
@@ -1,15 +1,13 @@
-// src/app/books.js
+// components/BookList.js
 "use client";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const BookList = () => {
   const [books, setBooks] = useState([]);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchBooks = async () => {
       try {
-        // Fetch data from the server
         const response = await fetch(
           "https://book-crud-service-6dmqxfovfq-et.a.run.app/api/books",
           {
@@ -17,41 +15,40 @@ const BookList = () => {
             headers: {
               Accept: "application/json",
               Authorization: `Bearer ${token}`,
+              // Include any necessary authorization headers
             },
           }
         );
 
-        const data = await response.json();
-
-        // Check if the 'data' property exists in the response
-        const booksData = data.data ? data.data : [];
-
-        setBooks(booksData);
-        setLoading(false);
+        if (response.ok) {
+          const data = await response.json();
+          setBooks(data);
+        } else {
+          console.error(
+            "Failed to fetch books:",
+            response.status,
+            response.statusText
+          );
+        }
       } catch (error) {
-        console.error("Error fetching data:", error);
-        setLoading(false);
+        console.error("Error fetching books:", error);
       }
     };
 
-    fetchData();
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+    fetchBooks();
+  }, []); // The empty dependency array ensures that the effect runs once when the component mounts
 
   return (
-    <div className="container mx-auto mt-8">
-      <h1 className="text-3xl font-bold mb-4">Book List</h1>
-      <div className="mb-4">
+    <div>
+      <h2>Book List</h2>
+      <ul>
         {books.map((book) => (
-          <div key={book.id} className=" shadow-md rounded p-6 mb-4">
-            <h2 className="text-xl font-semibold mb-2">{book.title}</h2>
+          <li key={book.id}>
+            <strong>{book.title}</strong> by {book.author}
             <p className="text-gray-700">{book.description}</p>
-          </div>
+          </li>
         ))}
-      </div>
+      </ul>
     </div>
   );
 };
